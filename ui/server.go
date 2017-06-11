8cd62dc069c61bb5e8cda4f59e2e028f2a205a20
Clifford Richardson <cmrallen@gmail.com>
2017-06-11 15:54:57 -0700

    Minor code addition on server subpackage

--- ui/server.go
+++ ui/server.go
@@ -1,4 +1,12 @@
 package ui
 
+import "net/http"
+
 // All things related to the user interface's web server go here. This excludes routes, which should be
 // placed into routes.go.
+
+//CreateServer uses the global configuration to create a server object that will be used for the Admin UI
+func CreateServer() (server *http.Server) {
+	server = new(http.Server)
+	return
+}
